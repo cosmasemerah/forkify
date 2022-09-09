278c50e3bf72695c6ab3e06d0456ba714705a3cd
costech95 <emerahcosmas@gmail.com>
2022-09-09 01:22:36 +0100

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '8f7c3731-d05a-411a-9f77-34fefdf91b76';
+export const MODAL_CLOSE_SEC = 2.5;
